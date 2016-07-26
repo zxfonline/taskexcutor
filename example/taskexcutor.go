7cd6174fd341e756f6a6607042011fca229bdd2e
zxfonline <zxfonline@sina.com>
2016-07-26 21:46:25 +0800

    change test class
    change test class

--- example/taskexcutor.go
+++ example/taskexcutor.go
@@ -19,12 +19,10 @@ func randInt(min int32, max int32) int32 {
 	return min + rand.Int31n(max-min)
 }
 
-var SysTimer *timer.Timer
 var logger = golog.New("ExcutorTest")
 
 func main() {
 	exc := taskexcutor.NewTaskExcutor(65536)
-	SysTimer = timer.NewTimer("", exc)
 	num := 10
 	for i := 0; i < num; i++ {
 		go func(i int) {
@@ -32,7 +30,7 @@ func main() {
 				name: fmt.Sprintf("name_%d", i+1),
 				t1:   T1{name: fmt.Sprintf("t1_%d", i+1)},
 			}
-			ev := SysTimer.AddTimerEvent(taskexcutor.NewTaskService(obj.t1.Processing), fmt.Sprintf("event1_%d", i+1), 0, time.Duration((100+int(randInt(1, 50))*i)*1e6), num+1-i, true)
+			ev := timer.AddTimerEvent(taskexcutor.NewTaskService(obj.t1.Processing), fmt.Sprintf("event1_%d", i+1), 0, time.Duration((100+int(randInt(1, 50))*i)*1e6), num+1-i, true)
 			obj.t1.event = ev
 		}(i)
 	}
